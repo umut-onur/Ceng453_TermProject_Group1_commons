8915da671fbc5959f2c0d17f9dc5a98baf8f166d
ONUR CELIK <e2309821@ceng.metu.edu.tr>
2022-06-23 01:45:32 +0300

    types: Make TradeOffer and TradeOfferMessage JSON-serializable

--- src/main/java/types/gameplay/TradeOffer.java
+++ src/main/java/types/gameplay/TradeOffer.java
@@ -1,8 +1,12 @@
 package types.gameplay;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import types.gameplay.Buyable;
 import types.gameplay.Player;
 import types.websocket.GameMessage;
+import types.websocket.TradeOfferMessage;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -15,12 +19,19 @@ public class TradeOffer implements GameEntity{
     private List<Buyable> buyablesOut;
     private int netBid;
     
+    public TradeOffer() {
+        this.sender = null;
+        this.receiver = null;
+        this.buyablesIn = new ArrayList<>();
+        this.buyablesOut = new ArrayList<>();
+        this.netBid = 0;
+    }
     public TradeOffer(Player sender, Player receiver) {
         this.sender = sender;
         this.receiver = receiver;
         this.buyablesIn = new ArrayList<>();
         this.buyablesOut = new ArrayList<>();
-        this. netBid = 0;
+        this.netBid = 0;
     }
     
     public TradeOffer(Player sender, Player receiver, List<Buyable> buyablesIn, List<Buyable> buyablesOut, int netBid) {
@@ -31,30 +42,57 @@ public class TradeOffer implements GameEntity{
         this.netBid = netBid;
     }
     
+    @JsonGetter
     public Player getSender() {
         return this.sender;
     }
     
+    @JsonGetter
     public Player getReceiver() {
         return this.receiver;
     }
     
+    @JsonGetter
     public List<Buyable> getBuyablesIn() {
         return this.buyablesIn;
     }
     
+    @JsonGetter
     public List<Buyable> getBuyablesOut() {
         return this.buyablesOut;
     }
     
+    @JsonGetter
     public int getNetBid() {
         return this.netBid;
     }
     
+    @JsonSetter
+    public void setSender(Player sender) {
+        this.sender = sender;
+    }
+    
+    @JsonSetter
+    public void setReceiver(Player receiver) {
+        this.receiver = receiver;
+    }
+    
+    @JsonSetter
+    public void setBuyablesOut(List<Buyable> buyablesOut) {
+        this.buyablesOut = buyablesOut;
+    }
+    
+    @JsonSetter
+    public void setBuyablesIn(List<Buyable> buyablesIn) {
+        this.buyablesIn = buyablesIn;
+    }
+    
+    @JsonSetter
     public void setNetBid(int netBid) {
         this.netBid = netBid;
     }
     
+    @JsonIgnore
     public boolean isValid() {
         boolean playersAreTogether = this.sender.getGameId().equals(this.receiver.getGameId());
         boolean senderOwnsAllOfferedBuyables = true;
@@ -74,11 +112,13 @@ public class TradeOffer implements GameEntity{
         return playersAreTogether && senderOwnsAllOfferedBuyables && receiverOwnsAllDemandedBuyables;
     }
     
+    @JsonIgnore
     @Override
     public String getGameId() {
         return this.sender.getGameId();
     }
     
+    @JsonIgnore
     public boolean equals(Object obj) {
         if (obj instanceof TradeOffer o) {
             return this.sender.is(o.sender) &&
--- src/main/java/types/websocket/TradeOfferMessage.java
+++ src/main/java/types/websocket/TradeOfferMessage.java
@@ -1,20 +1,28 @@
 package types.websocket;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import types.gameplay.Game;
 import types.gameplay.TradeOffer;
 
 public class TradeOfferMessage extends GameMessage {
     private TradeOffer offer;
     
+    public TradeOfferMessage() {
+        super();
+    }
+    
     public TradeOfferMessage(TradeOffer offer) {
         super(offer.getSender().getUserId(), offer.getSender().getGameId());
         this.offer = offer;
     }
     
+    @JsonGetter
     public TradeOffer getOffer() {
         return this.offer;
     }
     
+    @JsonSetter
     public void setOffer(TradeOffer offer) {
         this.offer = offer;
     }
