84c4618f21f7315960c853905d43306e67810bd4
ONUR CELIK <e2309821@ceng.metu.edu.tr>
2022-06-22 03:27:42 +0300

    rest/requests: modify JoinGameRequest so that it carries player name info

--- src/main/java/types/rest/requests/JoinGameRequest.java
+++ src/main/java/types/rest/requests/JoinGameRequest.java
@@ -6,14 +6,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class JoinGameRequest {
     private final String gameId;
+    private final String joiningUserPlayerName;
     
     @JsonCreator
     public JoinGameRequest(
-            @JsonProperty("gameId") String gameId
+            @JsonProperty("gameId") String gameId,
+            @JsonProperty("joiningUserPlayerName") String joiningUserPlayerName
     ) {
         this.gameId = gameId;
+        this.joiningUserPlayerName = joiningUserPlayerName;
     }
     
     @JsonGetter
     public String getGameId() { return gameId; }
+    
+    @JsonGetter
+    public String getJoiningUserPlayerName() { return joiningUserPlayerName; }
 }
